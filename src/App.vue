82fbb2bc89698f4f7360025430d04aad7a50e152
BuildTools <donalshortt@donals.eu>
2020-06-07 17:12:53 +0200

    fixed merging problems

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-=======
 <template lang="pug">
     div#base
         router-view
@@ -28,21 +11,9 @@ export default {
     components: {
         Home
     }
->>>>>>> 703e1b4d6f1aca0187063fbe5d05b415553eabf5
 }
 </script>
 
 <style>
-<<<<<<< HEAD
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-=======
-
->>>>>>> 703e1b4d6f1aca0187063fbe5d05b415553eabf5
+  
 </style>
