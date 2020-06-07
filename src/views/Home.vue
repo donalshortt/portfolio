703e1b4d6f1aca0187063fbe5d05b415553eabf5
donal <donalshortt@donals.eu>
2020-06-07 16:42:42 +0200

    initial

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    div#base
+        h1 Hello There my friend
+        router-link(to="/projects") Projects
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
